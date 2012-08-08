98af51a89ea37c03e99db7ba370913e455378b96
Theron Boerner <hunterboerner@gmail.com>
2012-08-07 22:27:37 -0500

    add death counter

--- src/me/hunterboerner/killCount/main.java
+++ src/me/hunterboerner/killCount/main.java
@@ -9,21 +9,41 @@ import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
 
 public class main extends org.bukkit.plugin.java.JavaPlugin implements Listener {
-	Map<String, Integer> players = new HashMap<String, Integer>();
-	Map<String, Integer> hostile = new HashMap<String, Integer>();
-	Map<String, Integer> neutral = new HashMap<String, Integer>();
-	Map<String, Integer> peaceful = new HashMap<String, Integer>();
+	Map<String, Integer> kills = new HashMap<String, Integer>();
+	Map<String, Integer> deaths = new HashMap<String, Integer>();
+	
 	
 	public void onEnable(){
 		getLogger().info("KillCount " + this.getDescription().getVersion() + " has been enabled.");
         getServer().getPluginManager().registerEvents(this, this);
         getServer().getMessenger().registerOutgoingPluginChannel(this, "KillCount");
+        getServer().getMessenger().registerOutgoingPluginChannel(this, "DeathCount");
 	}
 	
 	public void onDisable(){
 		getLogger().info("Kill Count " + this.getDescription().getVersion() + " has been enabled");
+	}
+	
+
+	
+	@EventHandler
+	public void onPlayerDeath(PlayerDeathEvent playerDeath){
+		String dead = playerDeath.getEntity().getName();
+		int deathCount = 0;
+		if(deaths.get(dead) != null){
+			deathCount = deaths.get(dead);
+		}
+		deathCount++;
+		deaths.put(dead, deathCount);
+		
+		
+		if (playerDeath.getEntity().getListeningPluginChannels().contains("KillCount")){
+			 String deathString = deathCount + "";
+			 playerDeath.getEntity().sendPluginMessage(this, "DeathCount", deathString.getBytes(java.nio.charset.Charset.forName("UTF-8")));
+		}
 	}
 	
 	@EventHandler
@@ -32,19 +52,20 @@ public class main extends org.bukkit.plugin.java.JavaPlugin implements Listener 
 		Player killer = death.getEntity().getKiller();
 		if(killer != null && killer.getGameMode() != GameMode.CREATIVE) {
 			int killCount = 0;
-			if(players.get(killer.getName()) != null) {
-				killCount = players.get(killer.getName());
+			if(kills.get(killer.getName()) != null) {
+				killCount = kills.get(killer.getName());
 			}
 			killCount++;
-			players.put(killer.getName(), killCount);
-			
-			String killString = killCount + "";
+			kills.put(killer.getName(), killCount);
 			
 			if (killer.getListeningPluginChannels().contains("KillCount"))	{
+				String killString = killCount + "";
 				killer.sendPluginMessage(this, "KillCount", killString.getBytes(java.nio.charset.Charset.forName("UTF-8")));
 			}
 		}
 		
 	}
+	
+	
 	
 }
